4d51cf0d8fc2243ad0b433c7e87565d72d51c353
bmcgavin <b.mcgavin@gmail.com>
2012-08-16 22:47:33 +0100

    Initial commit

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject spec/lein-template "0.1.0-SNAPSHOT"
+  :description "New project template for speclj"
+  :url "https://github.com/slagyr/speclj"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :eval-in-leiningen true)
